44927f94831e3d529107e34c7612b63a92099219
Han Yuri <dbfl0406@e-mirim.hs.kr>
2018-10-12 23:03:04 +0900

    onBindViewHolder of recyclerview holder

--- app/src/main/java/com/five/high/emirim/geulgil/geulgil/adapter/CardRecyclerAdapter.kt
+++ app/src/main/java/com/five/high/emirim/geulgil/geulgil/adapter/CardRecyclerAdapter.kt
@@ -1,9 +1,11 @@
 package com.five.high.emirim.geulgil.geulgil.adapter
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Button
 import com.five.high.emirim.geulgil.geulgil.R
 import com.five.high.emirim.geulgil.geulgil.alias.HolderType
 import com.five.high.emirim.geulgil.geulgil.holder.SamesoundsWrodHolder
@@ -14,15 +16,34 @@ import com.five.high.emirim.geulgil.geulgil.model.Word
  * Created by devuri on 2018-10-05.
  */
 class CardRecyclerAdapter(private val items : ArrayList<Word>, private val context: Context) : RecyclerView.Adapter<RecyclerView.ViewHolder>(){
+    @SuppressLint("SetTextI18n")
     override fun onBindViewHolder(holder:  RecyclerView.ViewHolder?, position: Int) {
         when{
             holder is SingleWordHolder -> {
+//              TODO
                 holder.tvWord?.text = items[position].word
-//                TODO
+                holder.tvMean?.text = items[position].means[0]
+
+                for(item in items[position].meanKeywords){
+                    holder.liMeanKeywords.addView(Button(context))
+                }
+
+                for(item in items[position].similarKeywords) {
+                    holder.liSimilarKeywords.addView(Button(context))
+                }
+
+                holder.liDelete.setOnClickListener{
+                    items.removeAt(position)
+                    notifyDataSetChanged()
+                }
             }
             holder is SamesoundsWrodHolder -> {
-//                TODO
-
+                holder.tvWord?.text = items[position].word
+                holder.tvSizeMessage?.text = "외 " + items[position].meanKeywords.size.toString() +"개의 결과"
+                holder.liDelete.setOnClickListener{
+                    items.removeAt(position)
+                    notifyDataSetChanged()
+                }
             }
 
         }
